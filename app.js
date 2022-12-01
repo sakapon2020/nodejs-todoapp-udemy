fcac7c039e52128c7276740b0efffbc60832dc27
sakapon2020 <sakamori77@msn.com>
2022-12-01 22:11:44 +0900

    deploy

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+app.use(express.json());
+app.use(express.static("./public"));
+
+// ルーティング設計
+const PORT = 5000;
+app.use("/api/v1/tasks", taskRoute);
+
+// データベースと接続
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(PORT, console.log("サーバーが起動しました"));
+    } catch (err) {
+        console.log(err);
+    }
+}
+start();
